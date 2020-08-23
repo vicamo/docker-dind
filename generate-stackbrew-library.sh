429ab8bda668be1ad1382fc9147f4369f51a3ee0
You-Sheng Yang <vicamo@gmail.com>
2020-08-23 23:15:55 +0800

    ci: update generate-stackbrew-library.sh

--- generate-stackbrew-library.sh
+++ generate-stackbrew-library.sh
@@ -1,44 +1,53 @@
 #!/bin/bash
 set -eu
 
-declare -A aliases=(
-	#[18.06]='edge'
-)
-
-# used for auto-detecting the "latest" of each channel (for tagging it appropriately)
-# https://blog.docker.com/2017/03/docker-enterprise-edition/
-declare -A latestChannelRelease=()
-
 self="$(basename "$BASH_SOURCE")"
 cd "$(dirname "$(readlink -f "$BASH_SOURCE")")"
 
 source '.architectures-lib'
 
 parentArches() {
-	local version="$1"; shift # "17.06", etc
+	local suite="$1"; shift # "17.06", etc
 
-	local parent="$(awk 'toupper($1) == "FROM" { print $2 }' "$version/Dockerfile")"
+	local parent="$(awk 'toupper($1) == "FROM" { print $2 }' "$suite/Dockerfile")"
 	echo "${parentRepoToArches[$parent]:-}"
 }
-versionArches() {
-	local version="$1"; shift
+suiteArches() {
+	local suite="$1"; shift
 
-	local parentArches="$(parentArches "$version")"
+	local parentArches="$(parentArches "$suite")"
 
-	local versionArches=()
+	local suiteArches=()
 	for arch in $parentArches; do
-		if hasBashbrewArch "$arch" && grep -qE "^# $arch\$" "$version/Dockerfile"; then
-			versionArches+=( "$arch" )
+		if hasBashbrewArch "$arch" && grep -qE "^# $arch\$" "$suite/Dockerfile"; then
+			suiteArches+=( "$arch" )
 		fi
 	done
-	echo "${versionArches[*]}"
+	echo "${suiteArches[*]}"
 }
 
-versions=( */ )
-versions=( "${versions[@]%/}" )
+debian="$(curl -fsSL 'https://raw.githubusercontent.com/docker-library/official-images/master/library/debian')"
+ubuntu="$(curl -fsSL 'https://raw.githubusercontent.com/docker-library/official-images/master/library/ubuntu')"
+suiteCodename() {
+	local suite="$1"; shift
+
+	if echo "$debian" | grep -qE "\b${suite}\b"; then
+		codename=$(curl -fsSL "http://deb.debian.org/debian/dists/${suite}/Release" | awk '/^Suite: / {print $2}')
+	elif echo "$ubuntu" | grep -qE "\b${suite}\b"; then
+		codename=$(curl -fsSL "http://archive.ubuntu.com/ubuntu/dists/${suite}/Release" | awk '/^Suite: / {print $2}')
+	else
+		echo >&2 "error: cannot determine repo for '$suite'"
+		exit 1
+	fi
+
+	[ "$suite" = "$codename" ] || echo "$codename"
+}
+
+flavors=( */*/ )
+flavors=( "${flavors[@]%/}" )
 
 # sort version numbers with highest first
-IFS=$'\n'; versions=( $(echo "${versions[*]}" | sort -rV) ); unset IFS
+IFS=$'\n'; flavors=( $(echo "${flavors[*]}" | sort -r) ); unset IFS
 
 # get the most recent commit which modified any of "$@"
 fileCommit() {
@@ -77,112 +86,68 @@ join() {
 	echo "${out#$sep}"
 }
 
-for version in "${versions[@]}"; do
-	rcVersion="${version%-rc}"
+for flavor in "${flavors[@]}"; do
+	suite=${flavor%/*}
+	arch=${flavor#*/}
 
-	commit="$(dirCommit "$version")"
+	commit="$(dirCommit "$flavor")"
 
-	fullVersion="$(git show "$commit":"$version/Dockerfile" | awk '$1 == "ENV" && $2 == "DOCKER_VERSION" { print $3; exit }')"
+	version="$(git show "$commit":"$flavor/Dockerfile" | awk '$1 == "ENV" && $2 == "DOCKER_VERSION" { print $3; exit }')"
 
-	if [ "$rcVersion" != "$version" ] && [ -e "$rcVersion/Dockerfile" ]; then
-		# if this is a "-rc" release, let's make sure the release it contains isn't already GA (and thus something we should not publish anymore)
-		rcFullVersion="$(git show HEAD:"$rcVersion/Dockerfile" | awk '$1 == "ENV" && $2 == "DOCKER_VERSION" { print $3; exit }')"
-		latestVersion="$({ echo "$fullVersion"; echo "$rcFullVersion"; } | sort -V | tail -1)"
-		if [[ "$fullVersion" == "$rcFullVersion"* ]] || [ "$latestVersion" = "$rcFullVersion" ]; then
-			# "x.y.z-rc1" == x.y.z*
-			continue
+	versionAliases=()
+	fullVersion=$version
+	while [ "${fullVersion%[.-]*}" != "$fullVersion" ]; do
+		versionAliases+=( $fullVersion )
+		fullVersion="${fullVersion%[.-]*}"
+	done
+	versionAliases+=(
+		$fullVersion
+	)
+
+	suiteAliases=( "${versionAliases[@]/%/-$suite}" )
+	codename=$(suiteCodename "${suite}")
+	if [ -n "$codename" ]; then
+		suiteAliases+=( "${versionAliases[@]/%/-$codename}" )
+		if [ "$codename" = "stable" ]; then
+			suiteAliases+=( "${versionAliases[@]/%/-latest}" )
 		fi
 	fi
 
-	versionAliases=()
-	if [ "$version" = "$rcVersion" ]; then
-		while [ "$fullVersion" != "$rcVersion" -a "${fullVersion%[.-]*}" != "$fullVersion" ]; do
-			versionAliases+=( $fullVersion )
-			fullVersion="${fullVersion%[.-]*}"
-		done
-	else
-		versionAliases+=( $fullVersion )
-	fi
-	versionAliases+=(
-		$version
-	)
-
-	# add a few channel/version-related aliases
-	channel="$(versionChannel "$version")"
-	majorVersion="${version%%.*}"
-	if [ "$version" != "$rcVersion" ] && [ -z "${latestChannelRelease['rc']:-}" ]; then
-		versionAliases+=( 'rc' )
-		latestChannelRelease['rc']="$version"
-	fi
-	if [ "$version" = "$rcVersion" ] && [ -z "${latestChannelRelease[$majorVersion]:-}" ]; then
-		versionAliases+=( "$majorVersion" )
-		latestChannelRelease["$majorVersion"]="$version"
-	fi
-	versionAliases+=(
-		${aliases[$version]:-}
-	)
-	if [ -z "${latestChannelRelease[$channel]:-}" ]; then
-		if [ "$rcVersion" = '19.03' ]; then versionAliases+=( "$channel" ); fi # 19.03 is the last release to include any "channel" aliases
-		latestChannelRelease[$channel]="$version"
-	fi
-	# every release goes into the "test" channel, so the biggest numbered release wins (RC or not)
-	if [ -z "${latestChannelRelease['test']:-}" ]; then
-		if [ "$rcVersion" = '19.03' ]; then versionAliases+=( 'test' ); fi # 19.03 is the last release to include any "channel" aliases
-		latestChannelRelease['test']="$version"
-	fi
-	if [ "$version" = "$rcVersion" ] && [ -z "${latestChannelRelease['latest']:-}" ]; then
-		versionAliases+=( 'latest' )
-		latestChannelRelease['latest']="$version"
-	fi
-
-	versionArches="$(versionArches "$version")"
+	suiteArches="$(suiteArches "$suite")"
 
 	echo
 	cat <<-EOE
-		Tags: $(join ', ' "${versionAliases[@]}")
-		Architectures: $(join ', ' $versionArches)
+		Tags: $(join ', ' "${suiteAliases[@]}")
+		Architectures: $(join ', ' $suiteArches)
 		GitCommit: $commit
-		Directory: $version
+		Directory: $flavor
 	EOE
 
-	for v in \
+	for variant in \
 		dind dind-rootless git \
-		windows/windowsservercore-{ltsc2016,1709} \
 	; do
-		dir="$version/$v"
+		dir="$flavor/$variant"
 		[ -f "$dir/Dockerfile" ] || continue
-		variant="$(basename "$v")"
 
 		commit="$(dirCommit "$dir")"
 
-		variantAliases=( "${versionAliases[@]/%/-$variant}" )
+		variantAliases=( "${suiteAliases[@]/%/-$variant}" )
 		variantAliases=( "${variantAliases[@]//latest-/}" )
 
-		case "$v" in
+		case "$variant" in
 			# https://github.com/docker/docker-ce/blob/8fb3bb7b2210789a4471c017561c1b0de0b4f145/components/engine/hack/make/binary-daemon#L24
 			# "vpnkit is amd64-only" ... for now??
 			dind-rootless) variantArches='amd64' ;;
 
-			windows/*) variantArches='windows-amd64' ;;
-			*)         variantArches="$versionArches" ;;
+			*)         variantArches="$suiteArches" ;;
 		esac
-
-		sharedTags=()
-		if [[ "$variant" == 'windowsservercore'* ]]; then
-			sharedTags=( "${versionAliases[@]/%/-windowsservercore}" )
-			sharedTags=( "${sharedTags[@]//latest-/}" )
-		fi
 
 		echo
 		echo "Tags: $(join ', ' "${variantAliases[@]}")"
-		if [ "${#sharedTags[@]}" -gt 0 ]; then
-			echo "SharedTags: $(join ', ' "${sharedTags[@]}")"
-		fi
 		cat <<-EOE
 			Architectures: $(join ', ' $variantArches)
 			GitCommit: $commit
 			Directory: $dir
 		EOE
-		[ "$variant" = "$v" ] || echo "Constraints: $variant"
 	done
 done
